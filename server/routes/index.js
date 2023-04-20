0b852cc3437a511a5f1c7cd29b372ac43ba6c131
malik <boughris@outlook.fr>
2023-04-20 23:23:26 +0100

    Added the search method

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,6 +6,7 @@ const API_BASE_URL = process.env.API_BASE_URL;
 
 router.get("/tags", async (req, res) => {
     try {
+        // Fetch all tags
         const response = await axios.get(`${API_BASE_URL}/api/tags`);
         const tags = await response.data;
 
@@ -17,18 +18,17 @@ router.get("/tags", async (req, res) => {
 });
 
 router.get("/cats/filter", async (req, res) => {
-    
     const { filtertag, omit, total } = req.query;
 
+    // Enforce queries
     if (!filtertag || !omit || !total) {
-        return res
-            .status(400)
-            .json({
-                error: "Bad Request",
-                messsage: "filtertag, omit and total parameters are required",
-            });
+        return res.status(400).json({
+            error: "Bad Request",
+            messsage: "filtertag, omit and total parameters are required",
+        });
     }
 
+    // Construct the params
     try {
         const params = new URLSearchParams({
             ["tags"]: filtertag,
@@ -36,6 +36,7 @@ router.get("/cats/filter", async (req, res) => {
             ["limit"]: total,
         });
 
+        // Fetch all cats that correspond to the params
         const response = await axios.get(`${API_BASE_URL}/api/cats?${params}`);
         const tags = await response.data;
 
@@ -43,6 +44,42 @@ router.get("/cats/filter", async (req, res) => {
     } catch (error) {
         console.error(error);
         res.status(500).json({ error });
+    }
+});
+
+router.get("/cats/match", async (req, res) => {
+    const { string } = req.query;
+    if (!string || typeof string !== "string") {
+        res.status(400).json({
+            error: "Invalid query parameter. 'substr' should be a non-empty string",
+        });
+        return;
+    }
+
+    try {
+        // Fetch all tags
+        const response1 = await axios.get(`${API_BASE_URL}/api/tags`);
+        const tags = response1.data;
+
+        // Filter tags based on substr
+        const filteredTags = tags.filter((tag) => tag.includes(string));
+
+        // Fetch cats that have any of the filtered tags
+        const params = new URLSearchParams({ tags: filteredTags.join(",") });
+        const response2 = await axios.get(`${API_BASE_URL}/api/cats?${params}`);
+        const cats = response2.data;
+
+        // Construct the response object
+        const responseObj = {
+            tags: filteredTags,
+            count: cats.length,
+            cats,
+        };
+
+        res.status(200).json(responseObj);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Something went wrong" });
     }
 });
 
